93ade4903db6b03746762ea2bc565c14d7574254
TXien <skyholy6@yahoo.com.tw>
2019-12-16 15:20:16 +0800

    change default link

--- bin/youtube-bot-linux.py
+++ bin/youtube-bot-linux.py
@@ -2,9 +2,9 @@ import time
 import webbrowser
 import os
 
-url = str(input("Enter your video url in ->\"url here \"<-  : "))
-n = input("Enter refresh rate(seconds) : ")
-brow = input("Enter your default browser in ->\"browser here \"<-  : ")
+url = "https://www.youtube.com/watch?v=D2aRUCRtJME"#str(input("Enter your video url in ->\"url here \"<-  : "))
+n = "3"#input("Enter refresh rate(seconds) : ")
+brow = "chrome"#input("Enter your default browser in ->\"browser here \"<-  : ")
 while (1):
     os.system(" killall -9 " + brow)
     time.sleep(int(n))
